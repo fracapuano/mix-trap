ae09626921630da4ad845843f10780d63845b876
fracapuano <francesco_capuano@aol.com>
2024-05-25 19:20:39 +0200

    enh: base speaker can search the web for info

--- modules/experts/base_speaker.py
+++ modules/experts/base_speaker.py
@@ -3,15 +3,30 @@ from modules.llm.mistral import (
     ChatMessage, 
     model
 )
+from modules.news.news_api import NewsHandler
+from modules.news.perplexity import topic_to_knowledge
+
 
 class BaseSpeaker:
     def __init__(self, topic: str):
         self.debate_topic = topic
         self.messages = []
         self.tools = []
+
+        # Add the news handler
+        self.news_client = NewsHandler()
     
     def add_message(self, content: str):
         self.messages.append(ChatMessage(role="user", content=content))
+    
+    def get_news_digest(self, topic: str):
+        news_data = self.news_client.create_news_report(topic)
+        return news_data
+    
+    def get_basic_knowledge(self, topic: str):
+        """Differently from news, this uses perplexity to generate a short summary of the topic."""
+        knowledge = topic_to_knowledge(topic)
+        return knowledge
     
     async def reply(self, observation:str):
         self.add_message(observation)
