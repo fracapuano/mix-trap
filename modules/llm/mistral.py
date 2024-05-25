6624b4fac4555d22aa75a4da132dda9d8f46dd65
Louis Sanna <louis.sanna@ekimetrics.com>
2024-05-25 12:08:48 +0200

    feat(mistral): init

---
+++ modules/llm/mistral.py
@@ -0,0 +1,25 @@
+import os
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+api_key = os.environ["MISTRAL_API_KEY"]
+model = "mistral-large-latest"
+
+client = MistralClient(api_key=api_key)
+
+
+def chat(messages):
+    chat_response = client.chat(
+        model=model,
+        messages=messages
+    )
+    return chat_response.choices[0].message.content
+
+
+if __name__ == '__main__':
+    result = chat([ChatMessage(role="user", content="What is the best French cheese?")])
+    print("result", result)
